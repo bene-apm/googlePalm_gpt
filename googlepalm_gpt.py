ccad5038999c0f905603f5b8c59e7b04b0e6734f
bene-apm <103486499+bene-apm@users.noreply.github.com>
2023-12-21 14:46:17 +0700

    Update googlepalm_gpt.py

--- googlepalm_gpt.py
+++ googlepalm_gpt.py
@@ -2,7 +2,7 @@ import sklearn
 import streamlit as st
 from langchain.llms import GooglePalm
 
-api_key = 'AIzaSyCKcyonzw4Z1CYwuNPo_lEjgQFrlqGJH_Y'
+api_key = 'Your API Key'
 
 llm = GooglePalm(google_api_key = api_key, temperature = 0.9)
 
