ff57dbc4c687fb96b407f04bbe1ae6e47f2178c1
bene-apm <103486499+bene-apm@users.noreply.github.com>
2023-12-25 02:41:37 +0700

    Add files via upload

--- googlepalm_gpt.py
+++ googlepalm_gpt.py
@@ -2,7 +2,7 @@ import sklearn
 import streamlit as st
 from langchain.llms import GooglePalm
 
-api_key = 'your api key here'
+api_key = 'AIzaSyCKcyonzw4Z1CYwuNPo_lEjgQFrlqGJH_Y'
 llm = GooglePalm(google_api_key = api_key, temperature = 0.9)
 
 st.title("Gpt using GooglePalm🌴")
