4ebd0ea1b798222686fa93d49d8371e48790afa0
bene-apm <103486499+bene-apm@users.noreply.github.com>
2023-12-25 02:36:11 +0700

    Update googlepalm_gpt.py

--- googlepalm_gpt.py
+++ googlepalm_gpt.py
@@ -1,8 +1,8 @@
 import sklearn
 import streamlit as st
 from langchain.llms import GooglePalm
-from config import api_key
 
+api_key = 'your api key here'
 llm = GooglePalm(google_api_key = api_key, temperature = 0.9)
 
 st.title("Gpt using GooglePalm🌴")
